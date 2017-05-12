0f44073d129fddd560f030977539620f44c9c389
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2017-05-13 00:26:08 +0530

    Add files via upload

--- flasktest.py
+++ flasktest.py
@@ -1,6 +1,6 @@
 from flask import * 
 
-app = Flask(__name__)                           #Flask constructor takes the name of current module (__name__) as argument.
+app = Flask(__name__)
 
 #@app.route(' URL ') works over the function written imediately below it 
 
@@ -10,26 +10,27 @@ def home():
 
 @app.route('/admin/')                               #visit localhost:5000/admin
 def adminpage():
-	return '<i>Welcome to Admin page</i>'
+	return '<i>Welcome Admin</i>'
 
-@app.route('/guest/<nameofguest>/')                   #visit localhost:5000/guest/<any argument to function>
-def guestpage(nameofguest):                           #this function accepts value to it's argument from URL
-	return '<b>Welcome to %s page </b>'%nameofguest
+@app.route('/guest/<nameofguest>/<contest>/')                   #visit localhost:5000/guest/<any argument to function>
+def guestpage(nameofguest,contest):                           #this function accepts value to it's argument from URL
+	return '<b>Welcome to %s page and %s contt</b>'%(nameofguest,contest)
 
-                                          #When a trailing slash (/) is used after an address, it becomes a canonical URL. 
-                                          #Hence, using /address or /address/ returns the same output. 
-                         #However, if code does not have trailing slash (/) in address, URL results in 404 Not Found page.
+"""Multiple arguments to a string format can be :
+   '%s for %s' %("tit", "tat"), 
+   '%(last)s, %(first)s %(last)s' %{'first': "James", 'last': "Bond"}"""
 
-@app.route('/users/<argument>/')                     
-def users(argument):                             
+"""When a trailing slash (/) is used after an address, it becomes a canonical URL. 
+Hence, using /address or /address/ returns the same output. 
+However, if code does not have trailing slash (/) in address, URL results in 404 Not Found page."""
+
+@app.route('/users/<argument>/<contestarg>')                     
+def users(argument,contestarg):                             
     if argument=="admin":                            #checks if argument is admin, sent to adminpage function
     	return redirect(url_for('adminpage'))
     else:                                           #else, sent to guestpage function with argument passed
-    	return redirect(url_for('guestpage',nameofguest=argument))
+    	return redirect(url_for('guestpage',nameofguest=argument,contest=contestarg))
 	
 if __name__ == '__main__':
 	app.debug = True                                  #to make changes dynamically in code and see results
 	app.run()
-
-                                                   #debugging mode:The server will then reload itself if the code changes. 
-                                   #It will also provide a useful debugger to track the errors if any, in the application.
