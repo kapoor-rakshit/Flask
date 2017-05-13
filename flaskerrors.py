17aaa5276fd31ed5266358849e725b3d5e6f819f
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2017-05-13 10:26:20 +0530

    Add files via upload

---
+++ flaskerrors.py
@@ -0,0 +1,49 @@
+from flask import *
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('login.html')
+
+@app.route('/check/', methods=["post"])
+def check():
+	if request.form['user']=="guest" and request.form['pass']=="guestpass":
+		return "Login Successful"
+	else:
+		return redirect(url_for('home'))                              #redirect to login page 
+
+#Flask.redirect(location, statuscode, response)
+#In the above function
+
+#location parameter is the URL where response should be redirected.
+#statuscode sent to browsers header, defaults to 302.
+#response parameter is used to instantiate response.
+
+#The following status codes are standardized :
+#HTTP_300_MULTIPLE_CHOICES
+#HTTP_301_MOVED_PERMANENTLY
+#HTTP_302_FOUND
+#HTTP_303_SEE_OTHER
+#HTTP_304_NOT_MODIFIED
+#HTTP_305_USE_PROXY
+#HTTP_306_RESERVED
+#HTTP_307_TEMPORARY_REDIRECT
+
+		#abort(401)                                                   #abort on call  
+
+#Flask.abort(code)
+#The Code parameter takes one of following values
+
+#400  for Bad Request
+#401  for Unauthenticated
+#403  for Forbidden
+#404  for Not Found
+#406  for Not Acceptable
+#415  for Unsupported Media Type
+#429  Too Many Requests"""
+
+if __name__=='__main__':
+	app.debug=True
+	app.run()
+
